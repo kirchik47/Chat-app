ce98913a75b9bb52061c36f1f278cc85fb07ad94
kirchik47 <104080425+kirchik47@users.noreply.github.com>
2022-08-04 11:33:05 +0300

    Add files via upload

---
+++ chat/new/models.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from django.db import models
+
+class Room(models.Model):
+    name = models.CharField(max_length=1000)
+class Message(models.Model):
+    value = models.CharField(max_length=100000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=100000)
+    room = models.CharField(max_length=100000)
